9e9705ea0e49467f74b94658fff343740b6b3c97
Myron Marston <myron.marston@gmail.com>
2013-09-13 17:08:08 -0700

    Tweak for my env.

--- test.sh
+++ test.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-RUBY_VERSION=1.9.3-p448
-SETUP="rvm ${RUBY_VERSION} do bundle exec"
-
 echo "Ruby, symlinked directory:"
-${SETUP} ruby bar/main.rb
+ruby bar/main.rb
 echo "Rspec, real directory:"
-${SETUP} rspec -o /dev/null foo/main.rb
+bin/rspec -o /dev/null foo/main.rb
 echo "Rspec, symlinked directory:"
-${SETUP} rspec -o /dev/null bar
+bin/rspec -o /dev/null bar
